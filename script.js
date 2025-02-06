0aa7ff2044d95902ea7f54e7ea473453ebcba4ed
fsx-bkt <175029590+fsx-bkt@users.noreply.github.com>
2025-02-06 21:30:10 +1000

    Refactoring code to be more consistent

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-let defaultGridSize = 16;
-let squareContainer = document.querySelector(".container");
-let changeGridButton = document.querySelector("#changeGridSize");
+ const defaultGridSize = 16;
+ const squareContainer = document.querySelector(".container");
+ const changeGridButton = document.querySelector("#changeGridSize");
 
 changeGridButton.addEventListener("click", changeGridSize);
 
 function changeGridSize(){
-    var newGridSize = prompt("Enter new number of squares per side:");
-    if (!Number.isInteger(newGridSize) && newGridSize <= 0){
+    let newGridSize = prompt("Enter new number of squares per side:");
+    if (!Number.isInteger(newGridSize) && newGridSize <= 0 && newGridSize > 100){
         alert("Please enter a number between 1 - 100!");
         return;
     }
 
     squareContainer.textContent = '';
-    MakeSquareGrid(newGridSize);
+    makeSquareGrid(newGridSize);
 }
-function MakeSquareGrid(gridSize){
+
+function makeSquareGrid(gridSize){
     let totalSquares = gridSize ** 2;
     let flexBasisModifier = (100 / gridSize) + '%';
     let counter = 0;
@@ -27,4 +28,5 @@ function MakeSquareGrid(gridSize){
         counter++;
     }
 }
-MakeSquareGrid(defaultGridSize);
+
+makeSquareGrid(defaultGridSize);
