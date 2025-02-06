e9de9e4c0ef2bf915379fd12ad48764dc05fa0a8
fsx-bkt <175029590+fsx-bkt@users.noreply.github.com>
2025-02-06 20:51:47 +1000

    Added option to change grid size responsively

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
+let defaultGridSize = 16;
 let squareContainer = document.querySelector(".container");
-let changeGridButton = document.querySelector("changeGridSize");
+let changeGridButton = document.querySelector("#changeGridSize");
 
-changeGridButton.addEventListener("click", changeGridSize());
+changeGridButton.addEventListener("click", changeGridSize);
 
 function changeGridSize(){
     var newGridSize = prompt("Enter new number of squares per side:");
-    
+    if (!Number.isInteger(newGridSize) && newGridSize <= 0){
+        alert("Please enter a number between 1 - 100!");
+        return;
+    }
+
+    squareContainer.textContent = '';
+    MakeSquareGrid(newGridSize);
 }
-function MakeSquareGrid(){
-    let number = 16 ** 2;
+function MakeSquareGrid(gridSize){
+    let totalSquares = gridSize ** 2;
+    let flexBasisModifier = (100 / gridSize) + '%';
     let counter = 0;
 
-    while(counter < number){
+    while(counter < totalSquares){
         let square = document.createElement("div");
         square.setAttribute("class", "square");
+        square.style.flexBasis = flexBasisModifier;
         squareContainer.appendChild(square);
         counter++;
     }
-
-    console.log(counter);
 }
-MakeSquareGrid();
+MakeSquareGrid(defaultGridSize);
